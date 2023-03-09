2371c9a3974e4a2b21d0be89b0cf5f575b02e735
timigyurina <timi.gyurina@gmail.com>
2023-03-09 10:16:37 +0100

    install necessary packages, create DbContext, add services to Program.cs

---
+++ Animal_Adoption_Management_System_Backend/Data/AnimalAdoptionContext.cs
@@ -0,0 +1,24 @@
+﻿using Animal_Adoption_Management_System_Backend.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Animal_Adoption_Management_System_Backend.Data
+{
+    public class AnimalAdoptionContext : DbContext
+    {
+        public AnimalAdoptionContext(DbContextOptions options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Animal> Animals { get; set; }
+        public DbSet<Shelter> Shelters { get; set; }
+        public DbSet<AnimalBreed> AnimalBreeds { get; set; }
+        public DbSet<Donation> Donations { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<AnimalShelter> AnimalShelters { get; set; }
+        public DbSet<AdoptionApplication> AdoptionApplications { get; set; }
+        public DbSet<AdoptionContract> AdoptionContracts { get; set; }
+        public DbSet<ManagedAdoptionContract> ManagedAdoptionContracts { get; set; }
+    }
+}
--- Animal_Adoption_Management_System_Backend/Program.cs
+++ Animal_Adoption_Management_System_Backend/Program.cs
@@ -1,25 +1,44 @@
+using Animal_Adoption_Management_System_Backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+string connectionString = builder.Configuration.GetConnectionString("AnimalAdoptionConnection") ?? throw new InvalidOperationException("Connection string 'AnimalAdoptionConnection' was not found.");
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AnimalAdoptionContext>(options =>
+    options.UseNpgsql(connectionString));
+
+
+builder.Services.AddCors(options => options.AddPolicy("AllowAll", builder =>
+{
+    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+}));
+
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
