c6760019000c6e4b973b4a03813795d9bc2ad099
timigyurina <timi.gyurina@gmail.com>
2023-03-16 10:25:07 +0100

    create GenericRepository interface and implementation

---
+++ Animal_Adoption_Management_System_Backend/Repositories/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace Animal_Adoption_Management_System_Backend.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T?> GetAsync(int? id);
+        Task<IEnumerable<T>> GetAllAsync(
+            Expression<Func<T, bool>>? filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            string includeProperties = ""
+        );
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+        Task<bool> Exists(int id);
+    }
+}
