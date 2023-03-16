83a6d68df2917bd1e6cf0f0c9ffe60451e8f2607
timigyurina <timi.gyurina@gmail.com>
2023-03-16 10:41:22 +0100

    Implement Unit of Work pattern
    Create IUnitOfWork interface and an implementation.
    Use UnitOfWork to have access to all services so that all Controllers will have to only know about the UnitOfWork, not the services themselves.

---
+++ Animal_Adoption_Management_System_Backend/Services/Implementations/UnitOfWork.cs
@@ -0,0 +1,55 @@
+﻿using Animal_Adoption_Management_System_Backend.Data;
+using Animal_Adoption_Management_System_Backend.Services.Interfaces;
+
+namespace Animal_Adoption_Management_System_Backend.Services.Implementations
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AnimalAdoptionContext _context;
+
+        private readonly IAnimalService _animalService;
+        private readonly IShelterService _shelterService;
+        private readonly IAnimalShelterService _animalShelterService;
+        private readonly IAnimalBreedService _animalBreedService;
+        private readonly IDonationService _donationService;
+        private readonly IImageService _imageService;
+        private readonly IAdoptionApplicationService _adoptionApplicationService;
+        private readonly IAdoptionContractService _adoptionContractService;
+        private readonly IManagedAdoptionContractService _managedAdoptionContractService;
+
+        public UnitOfWork(
+            AnimalAdoptionContext context, 
+            IAnimalService animalService, 
+            IShelterService shelterService, 
+            IAnimalShelterService animalShelterService, 
+            IAnimalBreedService animalBreedService, 
+            IDonationService donationService, 
+            IImageService imageService, 
+            IAdoptionApplicationService adoptionApplicationService, 
+            IAdoptionContractService adoptionContractService, 
+            IManagedAdoptionContractService managedAdoptionContractService)
+        {
+            _context = context;
+            _animalService = animalService;
+            _shelterService = shelterService;
+            _animalShelterService = animalShelterService;
+            _animalBreedService = animalBreedService;
+            _donationService = donationService;
+            _imageService = imageService;
+            _adoptionApplicationService = adoptionApplicationService;
+            _adoptionContractService = adoptionContractService;
+            _managedAdoptionContractService = managedAdoptionContractService;
+        }
+
+
+        public IAnimalService AnimalService => _animalService;
+        public IShelterService ShelterService => _shelterService;
+        public IAnimalShelterService AnimalShelterService => _animalShelterService;
+        public IAnimalBreedService AnimalBreedService => _animalBreedService;
+        public IDonationService DonationService => _donationService;
+        public IImageService ImageService => _imageService;
+        public IAdoptionApplicationService AdoptionApplicationService => _adoptionApplicationService;
+        public IAdoptionContractService AdoptionContractService => _adoptionContractService;
+        public IManagedAdoptionContractService ManagedAdoptionContractService => _managedAdoptionContractService;
+    }
+}
---
+++ Animal_Adoption_Management_System_Backend/Services/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿namespace Animal_Adoption_Management_System_Backend.Services.Interfaces
+{
+    public interface IUnitOfWork 
+    {
+        public IAnimalService AnimalService { get; }
+        public IShelterService ShelterService { get; }
+        public IAnimalShelterService AnimalShelterService { get; }
+        public IAnimalBreedService AnimalBreedService { get; }
+        public IDonationService DonationService { get; }
+        public IImageService ImageService { get; }
+
+        public IAdoptionApplicationService AdoptionApplicationService { get; }
+        public IAdoptionContractService AdoptionContractService { get; }
+        public IManagedAdoptionContractService ManagedAdoptionContractService { get; }
+    }
+}
