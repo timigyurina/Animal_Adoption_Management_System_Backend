5c06b74a728bbd122b177d543ac122befc6cd445
timigyurina <timi.gyurina@gmail.com>
2023-03-16 10:38:52 +0100

    Create service implementations
    These are made for specific actions with the enitity classes they serve.
    The services implement their corresponding interface. They also extend GenericRepository with the specific model class they serve.

---
+++ Animal_Adoption_Management_System_Backend/Services/Implementations/AnimalBreedService.cs
@@ -0,0 +1,14 @@
+﻿using Animal_Adoption_Management_System_Backend.Data;
+using Animal_Adoption_Management_System_Backend.Models.Entities;
+using Animal_Adoption_Management_System_Backend.Repositories;
+using Animal_Adoption_Management_System_Backend.Services.Interfaces;
+
+namespace Animal_Adoption_Management_System_Backend.Services.Implementations
+{
+    public class AnimalBreedService : GenericRepository<AnimalBreed>, IAnimalBreedService
+    {
+        public AnimalBreedService(AnimalAdoptionContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Animal_Adoption_Management_System_Backend/Services/Implementations/AnimalShelterService.cs
@@ -0,0 +1,14 @@
+﻿using Animal_Adoption_Management_System_Backend.Data;
+using Animal_Adoption_Management_System_Backend.Models.Entities;
+using Animal_Adoption_Management_System_Backend.Repositories;
+using Animal_Adoption_Management_System_Backend.Services.Interfaces;
+
+namespace Animal_Adoption_Management_System_Backend.Services.Implementations
+{
+    public class AnimalShelterService : GenericRepository<AnimalShelter>, IAnimalShelterService
+    {
+        public AnimalShelterService(AnimalAdoptionContext context) : base(context)
+        {
+        }
+    }
+}
