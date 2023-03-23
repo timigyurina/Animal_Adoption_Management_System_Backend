766644f220f8734a30761b6fcd57d01a99134c13
timigyurina <timi.gyurina@gmail.com>
2023-03-23 10:39:17 +0100

    add CreateAnimalShelterConnection method in IAnimalShelterService, implement in AnimalShelterService

--- Animal_Adoption_Management_System_Backend/Services/Implementations/AnimalShelterService.cs
+++ Animal_Adoption_Management_System_Backend/Services/Implementations/AnimalShelterService.cs
@@ -1,5 +1,6 @@
 ﻿using Animal_Adoption_Management_System_Backend.Data;
 using Animal_Adoption_Management_System_Backend.Models.Entities;
+using Animal_Adoption_Management_System_Backend.Models.Exceptions;
 using Animal_Adoption_Management_System_Backend.Repositories;
 using Animal_Adoption_Management_System_Backend.Services.Interfaces;
 
@@ -9,6 +10,21 @@ namespace Animal_Adoption_Management_System_Backend.Services.Implementations
     {
         public AnimalShelterService(AnimalAdoptionContext context) : base(context)
         {
+        }
+
+        public async Task<AnimalShelter> CreateAnimalShelterConnection(Animal animal, Shelter shelter, DateTime enrollmentDate)
+        {
+            if (ConnectionExists(animal.Id, shelter.Id, enrollmentDate))
+                throw new BadRequestException("Connection already exists");
+
+            AnimalShelter connection = new() { Animal = animal, Shelter = shelter, EnrollmentDate = enrollmentDate };
+            await AddAsync(connection);
+            return connection;
+        }
+
+        private bool ConnectionExists(int animalId, int shelterId, DateTime enrollmentDate)
+        {
+            return _context.AnimalShelters.Any(a => a.Animal.Id == animalId && a.Shelter.Id == shelterId && a.EnrollmentDate >= enrollmentDate);
         }
     }
 }
--- Animal_Adoption_Management_System_Backend/Services/Interfaces/IAnimalShelterService.cs
+++ Animal_Adoption_Management_System_Backend/Services/Interfaces/IAnimalShelterService.cs
@@ -5,5 +5,6 @@ namespace Animal_Adoption_Management_System_Backend.Services.Interfaces
 {
     public interface IAnimalShelterService : IGenericRepository<AnimalShelter>
     {
+        Task<AnimalShelter> CreateAnimalShelterConnection(Animal animal, Shelter shelter, DateTime enrollmentDate);
     }
 }
