feba10ef4b5210667b1579b93be4360d9ed0ebb5
timigyurina <timi.gyurina@gmail.com>
2023-03-23 10:50:41 +0100

    implement endpoints for CRUD operations in AnimalBreedController

--- Animal_Adoption_Management_System_Backend/Controllers/AnimalBreedController.cs
+++ Animal_Adoption_Management_System_Backend/Controllers/AnimalBreedController.cs
@@ -1,8 +1,11 @@
-﻿using Animal_Adoption_Management_System_Backend.Models.DTOs.AnimalBreed;
+﻿using Animal_Adoption_Management_System_Backend.Models.DTOs.AnimalBreedDTOS;
 using Animal_Adoption_Management_System_Backend.Models.Entities;
+using Animal_Adoption_Management_System_Backend.Models.Enums;
+using Animal_Adoption_Management_System_Backend.Models.Exceptions;
 using Animal_Adoption_Management_System_Backend.Services.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Animal_Adoption_Management_System_Backend.Controllers
 {
@@ -26,6 +29,73 @@ namespace Animal_Adoption_Management_System_Backend.Controllers
             IEnumerable<AnimalBreed> breeds = await _unitOfWork.AnimalBreedService.GetAllAsync();
             IEnumerable<AnimalBreedDTO> breedDTOs = _mapper.Map<IEnumerable<AnimalBreedDTO>>(breeds);
             return Ok(breedDTOs);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AnimalBreedDTO>> GetBreed(int id)
+        {
+            AnimalBreed? breed = await _unitOfWork.AnimalBreedService.GetAsync(id);
+            if (breed == null)
+                throw new NotFoundException(typeof(AnimalBreed).Name, id);
+
+            AnimalBreedDTO breedDTO = _mapper.Map<AnimalBreedDTO>(breed);
+            return Ok(breedDTO);
+        }
+
+        [HttpGet("filter")]
+        public async Task<ActionResult<IEnumerable<AnimalBreedDTO>>> GetFilteredBreeds(string? name, string? type, AnimalType? t)
+        {
+            //IEnumerable<AnimalBreed> breeds = await _unitOfWork.AnimalBreedService.GetFilteredBreedsAsync(name, type);
+            IEnumerable<AnimalBreed> breeds = await _unitOfWork.AnimalBreedService.GetFilteredBreedsAsync(name, t);
+            IEnumerable<AnimalBreedDTO> breedDTOs = _mapper.Map<IEnumerable<AnimalBreedDTO>>(breeds);
+            return Ok(breedDTOs);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AnimalBreedDTO>> CreateBreed(CreateAnimalBreedDTO animalBreedDTO)
+        {
+            AnimalBreed breedToCreate = _mapper.Map<AnimalBreed>(animalBreedDTO);
+            AnimalBreed createdBreed = await _unitOfWork.AnimalBreedService.AddAsync(breedToCreate);
+
+            AnimalBreedDTO createdBreedDTO = _mapper.Map<AnimalBreedDTO>(createdBreed);
+            return CreatedAtAction("GetBreed", new { id = createdBreed.Id }, createdBreedDTO);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<AnimalBreedDTO>> UpdateBreed(int id, CreateAnimalBreedDTO animalBreedDTO)
+        {
+            AnimalBreed? breedToUpdate = await _unitOfWork.AnimalBreedService.GetAsync(id);
+            if (breedToUpdate == null)
+                throw new NotFoundException(typeof(AnimalBreed).Name, id);
+
+            _mapper.Map(animalBreedDTO, breedToUpdate);
+
+            try
+            {
+                await _unitOfWork.AnimalBreedService.UpdateAsync(breedToUpdate);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await _unitOfWork.AnimalBreedService.Exists(id))
+                    throw new NotFoundException(typeof(AnimalBreed).Name, id);
+                else
+                    throw;
+            }
+
+            AnimalBreedDTO updatedBreed = _mapper.Map<AnimalBreedDTO>(breedToUpdate);
+            return Ok(updatedBreed);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBreed(int id)
+        {
+            AnimalBreed? breedToDelete = await _unitOfWork.AnimalBreedService.GetAsync(id);
+            if (breedToDelete == null)
+                throw new NotFoundException(typeof(AnimalBreed).Name, id);
+
+            await _unitOfWork.AnimalBreedService.DeleteAsync(id);
+
+            return NoContent();
         }
     }
 }
