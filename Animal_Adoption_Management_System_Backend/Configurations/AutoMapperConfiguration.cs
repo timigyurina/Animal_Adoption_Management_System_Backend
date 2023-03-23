e8dc299da9b4b9bf31080b5c0cb47cc980f357f7
timigyurina <timi.gyurina@gmail.com>
2023-03-23 10:49:59 +0100

    register DTOs in AutoMapperConfig

--- Animal_Adoption_Management_System_Backend/Configurations/AutoMapperConfiguration.cs
+++ Animal_Adoption_Management_System_Backend/Configurations/AutoMapperConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using Animal_Adoption_Management_System_Backend.Models.DTOs.AnimalBreed;
+﻿using Animal_Adoption_Management_System_Backend.Models.DTOs.AnimalBreedDTOS;
+using Animal_Adoption_Management_System_Backend.Models.DTOs.AnimalDTOs;
+using Animal_Adoption_Management_System_Backend.Models.DTOs.AnimalShelterDTOs;
+using Animal_Adoption_Management_System_Backend.Models.DTOs.ShelterDTOs;
 using Animal_Adoption_Management_System_Backend.Models.Entities;
 using AutoMapper;
 
@@ -9,6 +12,17 @@ namespace Animal_Adoption_Management_System_Backend.Configurations
         public AutoMapperConfiguration()
         {
             CreateMap<AnimalBreed, AnimalBreedDTO>().ReverseMap();
+            CreateMap<AnimalBreed, CreateAnimalBreedDTO>().ReverseMap();
+
+            CreateMap<Animal, CreateAnimalDTO>().ReverseMap();
+            CreateMap<Animal, AnimalDTO>().ReverseMap();
+            CreateMap<Animal, AnimalDTOWithDetails>().ReverseMap();
+            CreateMap<Animal, UpdateAnimalDTO>().ReverseMap();
+            
+            CreateMap<Shelter, CreateShelterDTO>().ReverseMap();
+            CreateMap<Shelter, ShelterDTO>().ReverseMap();
+
+            CreateMap<AnimalShelter, AnimalShelterDTO>().ReverseMap();
         }
     }
 }
