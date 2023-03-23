fb961c24369185c17dcad634a15a5bb0b5f67e64
timigyurina <timi.gyurina@gmail.com>
2023-03-23 10:23:43 +0100

    create Exception classes for custom middleware

---
+++ Animal_Adoption_Management_System_Backend/Models/Exceptions/ErrorDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace Animal_Adoption_Management_System_Backend.Models.Exceptions
+{
+    public class ErrorDetails
+    {
+        public string ErrorType { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
