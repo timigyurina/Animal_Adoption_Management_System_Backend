cdf3a3d67d8369a22bd8bf7803c83b5f25f1f74b
timigyurina <timi.gyurina@gmail.com>
2023-03-23 10:36:14 +0100

    Create methods for IAnimalService, implement in AnimalService
    Methods are for filtering, getting with details and updating various properties of Animal.

--- Animal_Adoption_Management_System_Backend/Services/Interfaces/IAnimalService.cs
+++ Animal_Adoption_Management_System_Backend/Services/Interfaces/IAnimalService.cs
@@ -1,9 +1,15 @@
-﻿using Animal_Adoption_Management_System_Backend.Models.Entities;
+﻿using Animal_Adoption_Management_System_Backend.Models.DTOs.AnimalDTOs;
+using Animal_Adoption_Management_System_Backend.Models.Entities;
+using Animal_Adoption_Management_System_Backend.Models.Enums;
 using Animal_Adoption_Management_System_Backend.Repositories;
 
 namespace Animal_Adoption_Management_System_Backend.Services.Interfaces
 {
     public interface IAnimalService : IGenericRepository<Animal>
     {
+        Task<IEnumerable<Animal>> GetFilteredAnimalsAsync(string? name, string? type, string? size, string? status, string? gender, string? color, int? breedId, bool? isSterilised, DateTime? bornAfter, DateTime? bornBefore);
+        Task<Animal> GetWithDetailsAsync(int id);
+        Task<Animal> UpdateSterilisation(int id, UpdateSterilisationDTO sterilisationDate);
+        Task<Animal> UpdateStatus(int id, AnimalStatus newStatus);
     }
 }
