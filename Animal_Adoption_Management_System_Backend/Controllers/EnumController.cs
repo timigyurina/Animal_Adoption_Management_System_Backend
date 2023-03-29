bf7697303b3da2ffb4f4c312ac2f10a44098e753
timigyurina <timi.gyurina@gmail.com>
2023-03-29 11:07:58 +0200

    create EnumController with endpoint for getting all values of an Enum type

---
+++ Animal_Adoption_Management_System_Backend/Controllers/EnumController.cs
@@ -0,0 +1,28 @@
+﻿using Animal_Adoption_Management_System_Backend.Models.Enums;
+using Animal_Adoption_Management_System_Backend.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Animal_Adoption_Management_System_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnumController : ControllerBase
+    {
+        private readonly IEnumService _enumService;
+
+        public EnumController(IEnumService enumService)
+        {
+            _enumService = enumService;
+        }
+
+
+        [HttpGet("{enumName}")]
+        public ActionResult<EnumDetails> GetEnum(string enumName)
+        {
+            EnumDetails enumDetails = _enumService.GetValuesOfEnum(enumName);
+            return Ok(enumDetails);
+        }
+    }
+}
