8c225aed65741d0ae7957bf160413baff70918e7
timigyurina <timi.gyurina@gmail.com>
2023-03-29 08:37:16 +0200

    Create methods for AdoptionContract creation and management flow
    Details:
    - Create methods in IAdoptionContractService, implement in AdoptionContractService. These methods are for getting with details, filtering, updating an AdoptionContract and checking related entities for the Contract creation.
    - Create methods in IManagedAdoptionContractService, implement in ManagedAdoptionContractService. These methods are for getting an AdoptionContract with manager details and for creating an AdoptionContract.

--- Animal_Adoption_Management_System_Backend/Services/Implementations/ManagedAdoptionContractService.cs
+++ Animal_Adoption_Management_System_Backend/Services/Implementations/ManagedAdoptionContractService.cs
@@ -1,7 +1,9 @@
 ﻿using Animal_Adoption_Management_System_Backend.Data;
 using Animal_Adoption_Management_System_Backend.Models.Entities;
+using Animal_Adoption_Management_System_Backend.Models.Exceptions;
 using Animal_Adoption_Management_System_Backend.Repositories;
 using Animal_Adoption_Management_System_Backend.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Animal_Adoption_Management_System_Backend.Services.Implementations
 {
@@ -9,6 +11,33 @@ namespace Animal_Adoption_Management_System_Backend.Services.Implementations
     {
         public ManagedAdoptionContractService(AnimalAdoptionContext context) : base(context)
         {
+        }
+
+        public async Task<ManagedAdoptionContract> GetByAdoptionContractIdAsync(int adoptionContractId)
+        {
+            if (!_context.ManagedAdoptionContracts.Any(m => m.Contract.Id == adoptionContractId))
+                throw new NotFoundException(typeof(AdoptionContract).Name, adoptionContractId);
+
+            ManagedAdoptionContract managedAdoptionContract = await _context.ManagedAdoptionContracts
+                .Include(m => m.Manager)
+                .AsNoTracking()
+                .FirstAsync(m => m.Contract.Id == adoptionContractId);
+
+            return managedAdoptionContract;
+        }
+
+        public async Task<ManagedAdoptionContract> TryAddRelatedEntitiesToManagedContract(string managerId, AdoptionContract adoptionContract)
+        {
+            User manager = await _context.Users
+                .FirstOrDefaultAsync(a => a.Id == managerId) ?? throw new NotFoundException($"{typeof(User).Name} (manager)", managerId);
+
+            ManagedAdoptionContract managedAdoptionContract = new()
+            {
+                Contract = adoptionContract,
+                Manager = manager
+            };
+
+            return managedAdoptionContract;
         }
     }
 }
