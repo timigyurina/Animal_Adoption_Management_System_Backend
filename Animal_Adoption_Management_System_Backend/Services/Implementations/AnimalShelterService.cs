04e4d69f4a536e849c1aba1472a52162ff542820
timigyurina <timi.gyurina@gmail.com>
2023-03-29 08:47:25 +0200

    Create CheckForAndCloseConnection method, refactor CreateAnimalShelterConnection
    AnimalShelter connection is closed (ExitDate is added to it)
    - either when an Animal is adopted (AdoptionContract is created)
    - or when the Animal is moved to an another Shelter (new AnimalShelter connection is created via calling addShelterConnection endpoint in AnimalController).

--- Animal_Adoption_Management_System_Backend/Services/Implementations/AnimalShelterService.cs
+++ Animal_Adoption_Management_System_Backend/Services/Implementations/AnimalShelterService.cs
@@ -3,6 +3,7 @@ using Animal_Adoption_Management_System_Backend.Models.Entities;
 using Animal_Adoption_Management_System_Backend.Models.Exceptions;
 using Animal_Adoption_Management_System_Backend.Repositories;
 using Animal_Adoption_Management_System_Backend.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Animal_Adoption_Management_System_Backend.Services.Implementations
 {
@@ -14,17 +15,31 @@ namespace Animal_Adoption_Management_System_Backend.Services.Implementations
 
         public async Task<AnimalShelter> CreateAnimalShelterConnection(Animal animal, Shelter shelter, DateTime enrollmentDate)
         {
-            if (ConnectionExists(animal.Id, shelter.Id, enrollmentDate))
-                throw new BadRequestException("Connection already exists");
+            if (ConnectionAlreadyExists(animal.Id, shelter.Id))
+                throw new BadRequestException("Connection between Animal and Shelter already exists");
 
-            AnimalShelter connection = new() { Animal = animal, Shelter = shelter, EnrollmentDate = enrollmentDate };
+            await CheckForAndCloseConnection(animal, enrollmentDate);
+
+            AnimalShelter connection = new() { Animal = animal, Shelter = shelter, EnrollmentDate = enrollmentDate, ExitDate = null };
             await AddAsync(connection);
             return connection;
         }
 
-        private bool ConnectionExists(int animalId, int shelterId, DateTime enrollmentDate)
+        public async Task CheckForAndCloseConnection(Animal animal, DateTime exitDate)
         {
-            return _context.AnimalShelters.Any(a => a.Animal.Id == animalId && a.Shelter.Id == shelterId && a.EnrollmentDate >= enrollmentDate);
+            AnimalShelter? openConnection = await _context.AnimalShelters
+                .FirstOrDefaultAsync(a => a.Animal == animal && a.ExitDate == null);
+            if (openConnection != null)
+            {
+                openConnection.ExitDate = exitDate;
+                await UpdateAsync(openConnection);
+            }
+        }
+
+        private bool ConnectionAlreadyExists(int animalId, int shelterId)
+        {
+            return _context.AnimalShelters
+                .Any(a => a.Animal.Id == animalId && a.Shelter.Id == shelterId && a.ExitDate == null);
         }
     }
 }
--- Animal_Adoption_Management_System_Backend/Services/Interfaces/IAnimalShelterService.cs
+++ Animal_Adoption_Management_System_Backend/Services/Interfaces/IAnimalShelterService.cs
@@ -5,6 +5,7 @@ namespace Animal_Adoption_Management_System_Backend.Services.Interfaces
 {
     public interface IAnimalShelterService : IGenericRepository<AnimalShelter>
     {
+        Task CheckForAndCloseConnection(Animal animal, DateTime exitDate);
         Task<AnimalShelter> CreateAnimalShelterConnection(Animal animal, Shelter shelter, DateTime enrollmentDate);
     }
 }
