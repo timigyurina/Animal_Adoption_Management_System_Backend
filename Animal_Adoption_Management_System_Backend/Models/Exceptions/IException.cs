7f1ee763c7fbea10b7594e20cb3ad06c17a9b74a
timigyurina <timi.gyurina@gmail.com>
2023-04-11 14:51:12 +0200

    refactor ExceptionMiddleware and Exceptions to depend on global interface (IException)

---
+++ Animal_Adoption_Management_System_Backend/Models/Exceptions/IException.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace Animal_Adoption_Management_System_Backend.Models.Exceptions
+{
+    public abstract class IException : Exception
+    {
+        protected IException(string? message) : base(message)
+        {
+        }
+
+        public abstract HttpStatusCode StatusCode { get; }
+        public abstract string ErrorType { get; }
+        public string Message => base.Message;
+    }
+}
--- Animal_Adoption_Management_System_Backend/Models/Exceptions/NotFoundException.cs
+++ Animal_Adoption_Management_System_Backend/Models/Exceptions/NotFoundException.cs
@@ -1,11 +1,15 @@
-﻿namespace Animal_Adoption_Management_System_Backend.Models.Exceptions
+﻿using System.Net;
+
+namespace Animal_Adoption_Management_System_Backend.Models.Exceptions
 {
-    public class NotFoundException : ApplicationException
+    public class NotFoundException : IException
     {
         // name is for where the key was not found and the value is for the value
         public NotFoundException(string name, object key) : base($"{name} with id ({key}) was not found")
         {
-
         }
+
+        public override HttpStatusCode StatusCode => HttpStatusCode.NotFound;
+        public override string ErrorType => "Not Found";
     }
 }
