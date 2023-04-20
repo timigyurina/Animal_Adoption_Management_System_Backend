916c0562af24de4c0bf3d6f8aae102199c2bf84e
timigyurina <timi.gyurina@gmail.com>
2023-04-20 10:53:53 +0200

    create classes for querying and returning paged results

---
+++ Animal_Adoption_Management_System_Backend/Models/Pagination/PagedResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Animal_Adoption_Management_System_Backend.Models.Pagination
+{
+    public class PagedResult<T>
+    {
+        public int TotalCount { get; set;} // all record (eg. 100)
+        public int CurrentPage { get; set; } // what page we are currently at (the client requested)
+        public int PageSize { get; set; } // max number of records that can be on the page (eg. 15 out of 100)
+        public List<T> Items { get; set; }
+    }
+}
---
+++ Animal_Adoption_Management_System_Backend/Models/Pagination/QueryParameters.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Animal_Adoption_Management_System_Backend.Models.Pagination
+{
+    public class QueryParameters
+    {
+        private int _pageSize = 15;
+        private int _pageNumber = 1;
+
+        public int StartIndex { get => PageSize * (PageNumber - 1); }
+
+        [Range(1, int.MaxValue)]
+        public int PageNumber // what page the client wants to get
+        { 
+            get { return _pageNumber; }
+            set { _pageNumber = value; }
+        }  
+
+        [Range(1, int.MaxValue)]
+        public int PageSize // how many items should be on page (default 15)
+        {
+            get { return _pageSize; }
+            set { _pageSize = value; }
+        }
+
+    }
+}
