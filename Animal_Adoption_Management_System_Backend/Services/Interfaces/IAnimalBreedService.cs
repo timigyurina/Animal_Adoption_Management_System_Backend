69b1d417e5049ad9f354a3450007825484a98d5e
timigyurina <timi.gyurina@gmail.com>
2023-05-09 20:05:49 +0200

    decide on filtering AnimalBreeds by AnimalType enum (not string) in IAnimalBreedService, also create GetPagedAndFilteredBreedsAsync method

--- Animal_Adoption_Management_System_Backend/Services/Interfaces/IAnimalBreedService.cs
+++ Animal_Adoption_Management_System_Backend/Services/Interfaces/IAnimalBreedService.cs
@@ -1,12 +1,14 @@
 ﻿using Animal_Adoption_Management_System_Backend.Models.Entities;
 using Animal_Adoption_Management_System_Backend.Models.Enums;
+using Animal_Adoption_Management_System_Backend.Models.Pagination;
 using Animal_Adoption_Management_System_Backend.Repositories;
 
 namespace Animal_Adoption_Management_System_Backend.Services.Interfaces
 {
     public interface IAnimalBreedService : IGenericRepository<AnimalBreed>
     {
-        Task<IEnumerable<AnimalBreed>> GetFilteredBreedsAsync(string? name, string? type);
-        Task<IEnumerable<AnimalBreed>> GetFilteredBreedsAsync(string? name, AnimalType? t);
+        Task<IEnumerable<AnimalBreed>> GetFilteredBreedsAsync(string? name, AnimalType? type);
+
+        Task<PagedResult<TResult>> GetPagedAndFilteredBreedsAsync<TResult>(QueryParameters queryParameters, string? name, AnimalType? type);
     }
 }
