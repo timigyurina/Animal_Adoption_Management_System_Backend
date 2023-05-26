c9876e28f2abe66e602eefd9ddf86240467db226
timigyurina <timi.gyurina@gmail.com>
2023-05-26 09:13:57 +0200

    create new DTOs and add to AutoMapperConfig

---
+++ Animal_Adoption_Management_System_Backend/Models/DTOs/ImageDTOs/ImageDTOWithAnimal.cs
@@ -0,0 +1,13 @@
+﻿using Animal_Adoption_Management_System_Backend.Models.DTOs.AnimalDTOs;
+
+namespace Animal_Adoption_Management_System_Backend.Models.DTOs.ImageDTOs
+{
+    public class ImageDTOWithAnimal : BaseImageDTO
+    {
+        public int Id { get; set; }
+        public DateTime DateTaken { get; set; }
+        public string ImagePath { get; set; }
+
+        public AnimalDTO Animal { get; set; }
+    }
+}
---
+++ Animal_Adoption_Management_System_Backend/Models/DTOs/ShelterDTOs/ShelterDTOWithAddress.cs
@@ -0,0 +1,11 @@
+﻿using Animal_Adoption_Management_System_Backend.Models.DTOs.AddressDTOs;
+
+namespace Animal_Adoption_Management_System_Backend.Models.DTOs.ShelterDTOs
+{
+    public class ShelterDTOWithAddress : BaseShelterDTO
+    {
+        public int Id { get; set; }
+        public bool IsActive { get; set; }
+        public AddressDTO Address { get; set; }
+    }
+}
