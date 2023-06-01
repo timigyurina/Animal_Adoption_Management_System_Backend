c93777d85827a9bdf2d5cb604d8c0bb5e93b80c6
timigyurina <timi.gyurina@gmail.com>
2023-06-01 09:09:10 +0200

    update endpoints that return entities belonging to user to use the UserId from the User's Claim

--- Animal_Adoption_Management_System_Backend/Controllers/UserController.cs
+++ Animal_Adoption_Management_System_Backend/Controllers/UserController.cs
@@ -1,16 +1,21 @@
 ﻿using Animal_Adoption_Management_System_Backend.Authorization;
 using Animal_Adoption_Management_System_Backend.Models.DTOs.AdoptionApplicationDTOs;
 using Animal_Adoption_Management_System_Backend.Models.DTOs.AdoptionContractDTOs;
+using Animal_Adoption_Management_System_Backend.Models.DTOs.AnimalDTOs;
+using Animal_Adoption_Management_System_Backend.Models.DTOs.AnimalShelterDTOs;
 using Animal_Adoption_Management_System_Backend.Models.DTOs.DonationDTOs;
 using Animal_Adoption_Management_System_Backend.Models.DTOs.ImageDTOs;
 using Animal_Adoption_Management_System_Backend.Models.DTOs.ManagedAdoptionContractDTOs;
+using Animal_Adoption_Management_System_Backend.Models.DTOs.ShelterDTOs;
 using Animal_Adoption_Management_System_Backend.Models.DTOs.UserDTOs;
 using Animal_Adoption_Management_System_Backend.Models.Entities;
+using Animal_Adoption_Management_System_Backend.Models.Exceptions;
 using Animal_Adoption_Management_System_Backend.Models.Pagination;
 using Animal_Adoption_Management_System_Backend.Services.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Animal_Adoption_Management_System_Backend.Controllers
 {
@@ -86,68 +91,103 @@ namespace Animal_Adoption_Management_System_Backend.Controllers
             return Ok(userDTOs);
         }
 
-        [HttpGet("{id}/donation")]
-        public async Task<ActionResult<ICollection<DonationDTOWithDetails>>> GetUserDonations(string id)
+        // All User has these entities
+        [HttpGet("donation")]
+        public async Task<ActionResult<ICollection<DonationDTOWithDetails>>> GetUserDonations()
         {
-            _permissionChecker.CheckPermissionForUserRelatedEntity(id, HttpContext.User, "Donation");
+            string? userIdClaimValue = Request.Cookies.FirstOrDefault(x => x.Key == "X-UserId").Value;
+            if (userIdClaimValue == null)
+                return Unauthorized();
 
-            User user = await _userService.GetWithDonationDetailsAsync(id);
+            User user = await _userService.GetWithDonationDetailsAsync(userIdClaimValue);
             UserDTOWithDetails userDTO = _mapper.Map<UserDTOWithDetails>(user);
             return Ok(userDTO.Donations);
         }
 
+        [HttpGet("adoptionApplication")]
+        public async Task<ActionResult<ICollection<AdoptionApplicationDTOWithDetails>>> GetUserAdoptionApplications()
+        {
+            string? userIdClaimValue = Request.Cookies.FirstOrDefault(x => x.Key == "X-UserId").Value;
+            if (userIdClaimValue == null)
+                return Unauthorized();
+
+            User user = await _userService.GetWithAdoptionApplicationDetailsAsync(userIdClaimValue);
+            IEnumerable<AdoptionApplicationDTOWithDetails> adoptionApplicationDTOs = _mapper.Map<IEnumerable<AdoptionApplicationDTOWithDetails>>(user.AdoptionApplications);
+            return Ok(adoptionApplicationDTOs);
+        }
+
+        [HttpGet("adoptionContract")]
+        public async Task<ActionResult<ICollection<AdoptionContractDTOWithManagerDetails>>> GetUserAdoptionContracts()
+        {
+            string? userIdClaimValue = Request.Cookies.FirstOrDefault(x => x.Key == "X-UserId").Value;
+            if (userIdClaimValue == null)
+                return Unauthorized();
+
+            User user = await _userService.GetWithAdoptionContractDetailsAsync(userIdClaimValue);
+            IEnumerable<AdoptionContractDTOWithDetails> adoptionContractDTOs = _mapper.Map<IEnumerable<AdoptionContractDTOWithDetails>>(user.AdoptionsContracts);
+            return Ok(adoptionContractDTOs);
+        }
+
+        // Only ShelterEmployee has these entites (they belong to the Sheter of the employee))
         [Authorize(Roles = "Administrator, ShelterEmployee")]
-        [HttpGet("{id}/image")]
-        public async Task<ActionResult<ICollection<ImageDTOWithDetails>>> GetUserImages(string id)
+        [HttpGet("shelter")]
+        public async Task<ActionResult<UserDTOWithDetails>> GetUserShelterInfo()
         {
-            _permissionChecker.CheckPermissionForUserRelatedEntity(id, HttpContext.User, "Image");
+            Claim? shelterIdClaim = User.Claims.FirstOrDefault(x => x.Type == "ShelterId") ?? throw new BadRequestException("User is not an Employee of any Shelter");
 
-            User user = await _userService.GetWithImageDetailsAsync(id);
-            UserDTOWithDetails userDTO = _mapper.Map<UserDTOWithDetails>(user);
-            return Ok(userDTO.Images);
+            Shelter shelterWithDetails = await _shelterService.GetWithDetailsAsync(int.Parse(shelterIdClaim.Value));
+            ShelterDTOWithDetails shelterDTOWithDetails = _mapper.Map<ShelterDTOWithDetails>(shelterWithDetails);
+            return Ok(shelterDTOWithDetails);
+        }
+        
+        [Authorize(Roles = "Administrator, ShelterEmployee")]
+        [HttpGet("shelter/animal")]
+        public async Task<ActionResult<UserDTOWithDetails>> GetUserShelterAnimals()
+        {
+            Claim? shelterIdClaim = User.Claims.FirstOrDefault(x => x.Type == "ShelterId") ?? throw new BadRequestException("User is not an Employee of any Shelter");
+
+            Shelter shelterWithAnimals = await _shelterService.GetWithAddressAndAnimalsAsync(int.Parse(shelterIdClaim.Value));
+            IEnumerable<AnimalShelterDTO> animals = _mapper.Map<IEnumerable<AnimalShelterDTO>>(shelterWithAnimals.Animals);
+            return Ok(animals);
+        }
+        
+        [Authorize(Roles = "Administrator, ShelterEmployee")]
+        [HttpGet("shelter/donation")]
+        public async Task<ActionResult<UserDTOWithDetails>> GetUserShelterDonations()
+        {
+            Claim? shelterIdClaim = User.Claims.FirstOrDefault(x => x.Type == "ShelterId") ?? throw new BadRequestException("User is not an Employee of any Shelter");
+
+            Shelter shelterWithDonations = await _shelterService.GetWithDonationsAsync(int.Parse(shelterIdClaim.Value));
+            IEnumerable<DonationDTOWithDetails> donations = _mapper.Map<IEnumerable<DonationDTOWithDetails>>(shelterWithDonations.Donations);
+            return Ok(donations);
         }
 
         [Authorize(Roles = "Administrator, ShelterEmployee")]
-        [HttpGet("{id}/shelter")]
-        public async Task<ActionResult<UserDTOWithDetails>> GetUserShelterInfo(string id)
+        [HttpGet("image")]
+        public async Task<ActionResult<ICollection<ImageDTOWithDetails>>> GetImagesUploadedByUser()
         {
-            _permissionChecker.CheckPermissionForUserRelatedEntity(id, HttpContext.User, "Shelter");
+            string? userIdClaimValue = Request.Cookies.FirstOrDefault(x => x.Key == "X-UserId").Value;
+            if (userIdClaimValue == null)
+                return Unauthorized();
 
-            User user = await _userService.GetWithShelterDetailsAsync(id);
-            UserDTOWithDetails userDTO = _mapper.Map<UserDTOWithDetails>(user);
-            return Ok(userDTO);
-        }
-
-        [HttpGet("{id}/adoptionApplication")]
-        public async Task<ActionResult<ICollection<AdoptionApplicationDTOWithDetails>>> GetUserAdoptionApplication(string id)
-        {
-            _permissionChecker.CheckPermissionForUserRelatedEntity(id, HttpContext.User, "AdoptionApplication");
-
-            User user = await _userService.GetWithAdoptionApplicationDetailsAsync(id);
-            UserDTOWithDetails userDTO = _mapper.Map<UserDTOWithDetails>(user);
-            return Ok(userDTO.AdoptionApplications);
-        }
-
-        [HttpGet("{id}/adoptionContract")]
-        public async Task<ActionResult<ICollection<AdoptionContractDTOWithManagerDetails>>> GetUserAdoptionContract(string id)
-        {
-            _permissionChecker.CheckPermissionForUserRelatedEntity(id, HttpContext.User, "AdoptionContract");
-
-            User user = await _userService.GetWithAdoptionContractDetailsAsync(id);
-            UserDTOWithDetails userDTO = _mapper.Map<UserDTOWithDetails>(user);
-            return Ok(userDTO.AdoptionsContracts);
+            User user = await _userService.GetWithImageDetailsAsync(userIdClaimValue);
+            IEnumerable<ImageDTOWithAnimal> imageDTOs = _mapper.Map<IEnumerable<ImageDTOWithAnimal>>(user.Images);
+            return Ok(imageDTOs);
         }
 
         [Authorize(Roles = "Administrator, ShelterEmployee")]
-        [HttpGet("{id}/managedAdoptionContract")]
-        public async Task<ActionResult<ICollection<ManagedAdoptionContractDTOWithDetails>>> GetUserManagedAdoptionContract(string id)
+        [HttpGet("managedAdoptionContract")]
+        public async Task<ActionResult<ICollection<ManagedAdoptionContractDTOWithDetails>>> GetAdoptionContractsManagedByUser()
         {
-            _permissionChecker.CheckPermissionForUserRelatedEntity(id, HttpContext.User, "ManagedAdoptionContract");
+            string? userIdClaimValue = Request.Cookies.FirstOrDefault(x => x.Key == "X-UserId").Value;
+            if (userIdClaimValue == null)
+                return Unauthorized();
 
-            User user = await _userService.GetWithManagedAdoptionContractDetailsAsync(id);
-            UserDTOWithDetails userDTO = _mapper.Map<UserDTOWithDetails>(user);
-            return Ok(userDTO.ManagedAdoptionsContracts);
+            User user = await _userService.GetWithManagedAdoptionContractDetailsAsync(userIdClaimValue);
+            IEnumerable<ManagedAdoptionContractDTOWithDetails> managedAdoptionContractDTOs = _mapper.Map<IEnumerable<ManagedAdoptionContractDTOWithDetails>>(user.ManagedAdoptionsContracts);
+            return Ok(managedAdoptionContractDTOs);
         }
+
 
         [Authorize(Roles = "Administrator")]
         [HttpPut("{id}/updateUserIsActive")]
